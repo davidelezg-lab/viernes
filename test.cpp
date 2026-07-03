d365d50ef2b0ece4141efa5f252347f982084441
davidelezg-lab <davidelezg@gmail.com>
2026-07-03 10:36:42 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
 
 int suma(int a, int b) {
-    return a + b;
+    return a - b;
 }
 
 int main() {
     if (suma(2, 3) != 5) {
         std::cerr << "ERROR: suma(2, 3) deberia devolver 5" << std::endl;
-        return 1;
-    }
-
-    if (suma(10, 20) != 30) {
-        std::cerr << "ERROR: suma(10, 20) deberia devolver 30" << std::endl;
         return 1;
     }
 
