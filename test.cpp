608cb15a67e6b32ce734598e02fc3bb6b642c121
davidelezg-lab <davidelezg@gmail.com>
2026-07-03 13:46:11 +0200

    traca final

--- test.cpp
+++ test.cpp
@@ -13,34 +13,35 @@ int multiplicar(int a, int b) {
 }
 
 int dividir(int a, int b) {
+
+    if (b == 0) {
+        return 0;
+    }
+
     return a / b;
 }
 
 int main() {
 
     // SUMA
-    if (suma(2, 3) != 5) {
-        std::cerr << "ERROR SUMA" << std::endl;
-        return 1;
-    }
+
+    if (suma(2, 3) != 5) return 1;
+    if (suma(-2, 2) != 0) return 1;
 
     // RESTA
-    if (resta(10, 4) != 6) {
-        std::cerr << "ERROR RESTA" << std::endl;
-        return 1;
-    }
+
+    if (resta(10, 4) != 6) return 1;
+    if (resta(5, 8) != -3) return 1;
 
     // MULTIPLICAR
-    if (multiplicar(3, 4) != 12) {
-        std::cerr << "ERROR MULTIPLICAR" << std::endl;
-        return 1;
-    }
+
+    if (multiplicar(3, 4) != 12) return 1;
+    if (multiplicar(5, 0) != 0) return 1;
 
     // DIVIDIR
-    if (dividir(20, 4) != 5) {
-        std::cerr << "ERROR DIVIDIR" << std::endl;
-        return 1;
-    }
+
+    if (dividir(20, 4) != 5) return 1;
+    if (dividir(10, 0) != 0) return 1;
 
     std::cout << "Todos los tests han pasado correctamente" << std::endl;
 
