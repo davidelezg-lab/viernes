24a763f684c99bb0412b757db6cb41cb3f1cbb02
davidelezg-lab <davidelezg@gmail.com>
2026-07-03 11:04:23 +0200

    Provocar código de baja calidad

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 
 int suma(int a, int b) {
-    return a - b;
+    return a + b;
 }
 
 int main() {
-    int resultado = suma(10, 20);
-    std::cout << "Resultado: " << resultado << std::endl;
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+    int e = 50;
+
+    if (a > 0) {
+        if (b > 0) {
+            if (c > 0) {
+                if (d > 0) {
+                    if (e > 0) {
+                        std::cout << suma(a, b) + c + d + e << std::endl;
+                    }
+                }
+            }
+        }
+    }
+
     return 0;
 }
