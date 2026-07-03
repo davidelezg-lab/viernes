ebf15289cb763b06a73670679e5e2756b95868fa
davidelezg-lab <davidelezg@gmail.com>
2026-07-03 10:18:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+
+int suma(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int resultado = suma(10, 20);
+    std::cout << "Resultado: " << resultado << std::endl;
+    return 0;
+}
 
